8841ee847ef9ec3d17f5c4742adf2cd3ed44b045
SymbolixAU <dcooley@symbolix.com.au>
2019-03-01 08:27:59 +1100

    initial commit

---
+++ R/geojsonvt-package.R
@@ -0,0 +1,10 @@
+#' Provides access to Mapbox Geojson-vt-cpp library
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # Include a dependency to geojsonvt inside a cpp file
+#' // [[Rcpp::depends(geojsonvt)]]
+#' include <Rcpp.h>
+#' }
+"_PACKAGE"
